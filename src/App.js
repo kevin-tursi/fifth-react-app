17ea398c4d9f9c0dff3d26a841c749b1a4f4b9d8
Kevin Tursi <kevin.tursi@uconn.edu>
2023-06-06 14:35:33 -0400

    added useEffect() for login info in local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
@@ -7,13 +7,22 @@ import MainHeader from './components/MainHeader/MainHeader';
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+
+  if (storedUserLoggedInInfo === 'LOGGED_IN') {
+    setIsLoggedIn(true);
+  }}, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', 'LOGGED_IN');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
