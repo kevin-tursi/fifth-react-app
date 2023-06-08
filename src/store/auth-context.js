7fe12dcee903786dfea26ea809c502cb50392b84
Kevin Tursi <kevin.tursi@uconn.edu>
2023-06-08 18:20:20 -0400

    Added App.js functionality to auth-context.js to have a leaner App.js file

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+    
+      if (storedUserLoggedInInfo === 'LOGGED_IN') {
+        setIsLoggedIn(true);
+      }}, []);
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    }
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', 'LOGGED_IN');
+        setIsLoggedIn(true);
+    }
+
+    return <AuthContext.Provider value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler
+
+    }}>{props.children}</AuthContext.Provider>
+}
 
 export default AuthContext;
